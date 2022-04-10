19d2f52f4580e95bfb6195e08e7d4836d4da8e44
lowe <lowe.raivio@gmail.com>
2022-04-10 14:39:21 +0200

    moved scripts, moved generation_proto stuff

--- void_rush/src/Main.cpp
+++ void_rush/src/Main.cpp
@@ -4,11 +4,12 @@
 #include <fstream>
 #include <time.h>
 #include "physics/proto.hpp"
+#include "generation/proto.hpp"
 
 int main()
 {
     physics_proto();
-    
+    generation_proto();
 
     return 0;
 }
