6a09327eedf5107c2967325cf4ec422b1ddb2b97
Oscar Sandberg <oscar.c.sandberg@gmail.com>
2022-04-11 16:19:41 +0200

    3d kaströrelse func

--- void_rush/src/common/Helper.cpp
+++ void_rush/src/common/Helper.cpp
@@ -1,4 +1,5 @@
 #include "Helper.hpp"
+#include <corecrt_math.h>
 
 float deg_to_rad (float deg)
 {
@@ -11,6 +12,15 @@ Vector2 Projectile(float rad, float time, Vector2 velocity)
     Vector2 pos;
     pos.x = velocity.x*cos(rad)*time;
     pos.y = (velocity.y*sin(rad))*time - (9.82*pow(time,2))/2;
+    return pos;
+}
+
+Vector3 Projectile(float rad_x, float rad_z, float time, Vector3 velocity)
+{
+    Vector3 pos;
+    pos.x = velocity.x*cos(rad_x)*time*cos(rad_z);
+    pos.y = (velocity.y*sin(rad_z))*time - (9.82*pow(time,2))/2;
+    pos.z = velocity.z*cos(rad_x)*time*sin(rad_z);
     return pos;
 }
 
