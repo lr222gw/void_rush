601f538a54fe491d767f03cda40d50c2f5d8f349
Oscar Sandberg <oscar.c.sandberg@gmail.com>
2022-04-11 14:17:00 +0200

    Lagt kastfunktion i helper

--- void_rush/src/common/Helper.cpp
+++ void_rush/src/common/Helper.cpp
@@ -5,3 +5,12 @@ float deg_to_rad (float deg)
     float ret = deg * (M_PI / 180);
     return ret;
 }
+
+Vector2 Projectile(float rad, float time, Vector2 velocity)
+{
+    Vector2 pos;
+    pos.x = velocity.x*cos(rad)*time;
+    pos.y = (velocity.y*sin(rad))*time - (9.82*pow(time,2))/2;
+    return pos;
+}
+
--- void_rush/src/common/Helper.hpp
+++ void_rush/src/common/Helper.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include "Vector.hpp"
 
 float deg_to_rad (float deg);
+
+Vector2 Projectile(float rad, float time, Vector2 velocity);
