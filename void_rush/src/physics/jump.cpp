f5534a3ea0ef3f2e72a8824ed131a12a543a76d9
OSCARNORDSTRÖM <osno19@student.bth.se>
2022-04-11 12:58:15 +0200

    Jump cleanup

--- void_rush/src/physics/jump.cpp
+++ void_rush/src/physics/jump.cpp
@@ -9,29 +9,27 @@ void test::Start ()
              << "\n";
 
     // Run
-
     Run (csv_file);
-
     csv_file.close ();
 }
 Vector3 test::GetPushVector (Vector3 enemyPos, Vector3 playerPos)
 {
     return playerPos - enemyPos;
 }
-Vector3 test::GetPos () { return position; }
+Vector3 test::GetPos () const { return position; }
 void test::Move (Vector3 step) { position += step; }
 void test::SetPos (Vector3 pos) { position = pos; }
-void test::SetVelocity (Vector3 v) { velocity = v; }
+void test::SetVelocity (Vector3 vec) { velocity = vec; }
 void test::Update (float deltaT)
 {
     if (!grounded)
     {
         // Update forces
-        force_g = gravityAcceleration * mass;
+        force_g = gravityAcceleration * MASS;
         // Set resulting force
         resultingForce = force_g;
         // Update acceleration
-        acceleration = resultingForce / mass;
+        acceleration = resultingForce / MASS;
         // Update velocity
         velocity = velocity + acceleration * deltaT;
     }
@@ -46,7 +44,6 @@ void test::Update (float deltaT)
             velocity.z -= frictionCheat;
         }
     }
-
     // Update position
     position = position + velocity * deltaT;
 }
@@ -61,23 +58,20 @@ void test::Jump ()
         velocity = Vector3 (velocity.x, jumpSpeed.y, velocity.z);
     }
 }
-void test::WriteToFile (std::ofstream &csv_file)
+void test::WriteToFile (std::ofstream &csv_file) const
 {
     csv_file << time << "," << position.x << "," << position.y << "\n";
 }
 void test::Run (std::ofstream &csv_file)
 {
-    SetVelocity (Vector3 (60.0f, 60.0f, 0.0f));
-    while (time < 21)
+    SetVelocity (Vector3 (START_V, START_V, 0.0f));
+    while (time < RUN_TIME)
     {
-
-        if (time == 8 || time == 13)
+        if (time == JMP_1 || time == JMP_2)
         {
             Jump ();
         }
-
         Update (deltlaT);
-
         WriteToFile (csv_file);
         time++;
     }
