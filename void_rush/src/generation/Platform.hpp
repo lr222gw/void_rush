a6eb922455bf7e77759dda29ec0cca63b6702c80
Nils.M <nimr20@student.bth.se>
2022-04-14 11:36:14 +0200

    Auto stash before rebase of "origin/3D_engine"

--- void_rush/src/generation/Platform.hpp
+++ void_rush/src/generation/Platform.hpp
@@ -1,11 +1,10 @@
 #pragma once
-#include "../common/Vector.hpp"
-#include "../common/Shape.hpp"
 #include "../common/Matrix.hpp"
+#include "../common/Shape.hpp"
+#include "../common/Vector.hpp"
 #include <math.h>
 #include <memory>
 #include <vector>
-
 
 class platform
 {
@@ -15,6 +14,8 @@ class platform
     int difficluty;
     double rotation;
     Shape platformShape;
+    RotationMatrix rotMat;
+
   public:
     platform ();
     platform (std::vector<float> pos, int obstacles, int difficluty, double rotation = 0.0);
@@ -24,6 +25,7 @@ class platform
     void setPosition (Vector3 position);
     void move (float xOfset, float yOfset, float zOfset);
     Vector3 getPos ();
+    double getRotation () const;
     float distance (Vector3 position) const;
     float distance (std::vector<float> &position) const;
     platform *next;
