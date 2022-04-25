aca5b1d17b887cdf88a13e194b33127fa29247ae
lowe <lowe.raivio@gmail.com>
2022-04-25 10:18:26 +0200

    Disabled particles

--- void_rush/src/3Dproj/Game.cpp
+++ void_rush/src/3Dproj/Game.cpp
@@ -44,7 +44,7 @@ Game::Game(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWS
 	
 	
 
-	setUpParticles();
+	//setUpParticles();
 	if (IMGUI) {
 		//UIManager.takeObject(obj[2]);
 		//UIManager.takeObject(obj[3]);
