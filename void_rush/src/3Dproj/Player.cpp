5cfc139628ef341e0f6ee824add2f1673efe187f
Joaquin <joaquinzache@gmail.com>
2022-04-26 14:04:42 +0200

    Missed taking away this->isKeyPressed = true for 'S' and 'A'

--- void_rush/src/3Dproj/Player.cpp
+++ void_rush/src/3Dproj/Player.cpp
@@ -257,7 +257,6 @@ void Player::handleEvents(float dt)
 		}
 	}
 	if (keyboard->isKeyPressed('S') && !this->isKeyPressed) {
-		this->isKeyPressed = true;
 		cam->calcFURVectors();
 		jumpDir = jumpDir + vec2(-cam->getForwardVec().x,  -cam->getForwardVec().z);
 		
@@ -344,7 +343,6 @@ void Player::handleEvents(float dt)
 		}
 	}
 	if (keyboard->isKeyPressed('A') && !this->isKeyPressed) {
-		this->isKeyPressed = true;
 		cam->calcFURVectors();
 		jumpDir = jumpDir + vec2(-cam->getRightVector().x, -cam->getRightVector().z);
 		
