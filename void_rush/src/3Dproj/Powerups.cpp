9b2668b3b3e68999a1ce5b30cff9916c6f3002fe
Oscar Sandberg <oscar.c.sandberg@gmail.com>
2022-05-03 11:23:50 +0200

    powerups construktor

--- void_rush/src/3Dproj/Powerups.cpp
+++ void_rush/src/3Dproj/Powerups.cpp
@@ -1,5 +1,14 @@
 #include "Powerups.h"
 
+Powerups::Powerups(Player* player, Hud* hud, ModelObj* file, Graphics*& gfx, vec3 pos, vec3 rot, vec3 scale) 
+	: player(player), hud(hud), GameObject(file, gfx, pos, rot, scale)
+{
+}
+
+Powerups::~Powerups()
+{
+}
+
 void Powerups::UsePowerUp()
 {
 	//if (PowerUp == EMPTY)
