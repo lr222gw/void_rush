7ced9afc3eec5557998d7178acae6e5221bd6acd
OSCARNORDSTRÖM <osno19@student.bth.se>
2022-05-03 16:31:06 +0200

    Sound for player shoved

--- void_rush/src/3Dproj/Player.cpp
+++ void_rush/src/3Dproj/Player.cpp
@@ -607,6 +607,7 @@ void Player::shovePlayer(vec2 shove, float forceY)
 	this->shoved = true;
 	this->shove = shove;
 	this->velocity.y = forceY;
+	sm->playSound("Shoved", getPos());
 	ResetGhost();
 }
 
