a789d19eb3462fc4ec44963e9bf9056521dcf1e1
Simon Strand <etta11@hotmail.se>
2022-05-06 10:45:41 +0200

    Yes it works

--- void_rush/src/3Dproj/Game.cpp
+++ void_rush/src/3Dproj/Game.cpp
@@ -209,10 +209,10 @@ GameStatesEnum Game::update(float dt)
 
 #pragma endregion camera_settings
 
-	/*
+	
 	if (getkey('L'))
 	{
-		HUD->UpdateScore(10000);
+		HUD->UpdateScore(100);
 	}
 
 	if (getkey('V') && testTime <= 0.0f)
@@ -244,7 +244,7 @@ GameStatesEnum Game::update(float dt)
 			HUD->TurnOnPassive(4);
 		}
 	}
-	*/
+	
 
 		testPuzzle->UpdatePlayerPosition(this->player->getPos());
 
@@ -436,6 +436,7 @@ void Game::setUpObject()
 	generationManager->initialize();
 	testPuzzle->Initiate(generationManager->getPuzzelPos()); 
 	//generationManager->initialize(); //NOTE: this should be done later, but is currently activated through IMGUI widget
+	distanceFromStartPosToPuzzle = generationManager->getPuzzelPos().length();
 	setUpPowerups(1, vec3(10, 10, 10));
 	setUpPowerups(1, vec3(15, 10, 15));
 	setUpPowerups(1, vec3(20, 10, 20));
@@ -600,7 +601,7 @@ void Game::setUpPowerups(int chosenDiff, vec3 pos)
 				else if (chosenPower > 94 && chosenPower <= 96)
 				{
 					//Pearl
-					GameObjManager->getGameObject("kill")->setPos(pos);
+					GameObjManager->getGameObject("Kill")->setPos(pos);
 				}
 				else if (chosenPower > 96 && chosenPower <= 98)
 				{
@@ -677,7 +678,7 @@ void Game::setUpPowerups(int chosenDiff, vec3 pos)
 				else if (chosenPower > 94 && chosenPower <= 96)
 				{
 					//Pearl
-					GameObjManager->getGameObject("kill")->setPos(pos);
+					GameObjManager->getGameObject("Kill")->setPos(pos);
 				}
 				else if (chosenPower > 96 && chosenPower <= 98)
 				{
