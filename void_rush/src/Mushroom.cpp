8ca5af8ea942eda0c7a50d42aa90261924f3c574
OSCARNORDSTRÖM <osno19@student.bth.se>
2022-05-17 09:48:13 +0200

    More mushroom fix

--- void_rush/src/Mushroom.cpp
+++ void_rush/src/Mushroom.cpp
@@ -19,27 +19,30 @@ void Mushroom::update(float dt)
 void Mushroom::collidedWithPlayer()
 {
 	vec3 horPush = GetMidPos(player) - GetMidPos(this);
-	float objPlayerLen = (GetMidPos(player) - GetMidPos(this)).length();
+	float objPlayerLen = (vec2(GetMidPos(player).x, GetMidPos(player).z) - vec2(GetMidPos(this).x, GetMidPos(this).z)).legth();
 	horPush.y = 0.0f;
 
 	DirectX::XMFLOAT4 bb[2];
 	this->getBoundingBox(bb);
-	float length = fabs(bb[0].x - bb[1].x);//(vec3(bb[0]) - vec3(bb[1])).length();
+	float length = fabs(bb[0].x - bb[1].x);
 	float radius = length / 2;
+
+	if (objPlayerLen > radius) {
+		return;
+	}
 
 	float howMuch = objPlayerLen / radius;
 	if (howMuch > 1) {
 		howMuch = 1;
 	}
 
+	float forceY = 20.0f;
 	float force = 10.0f;
 
-	float yPush = (1 - howMuch) * objPlayerLen * force;
+	float yPush = (1 - howMuch) * forceY;
 	horPush = horPush * force;
 
-	if (objPlayerLen > radius) {
-		return;
-	}
+	
 
 	player->shovePlayer(vec2(horPush.x, horPush.z), yPush);
 	
