42c1106df04497106cf91f4d73ef8951c110ad34
Oscar Sandberg <oscar.c.sandberg@gmail.com>
2022-05-17 10:17:57 +0200

    intelisense

--- void_rush/src/3Dproj/Player.h
+++ void_rush/src/3Dproj/Player.h
@@ -30,7 +30,7 @@ enum PowerUpPassiv
 
 class Player : public GameObject {
 public:
-	Player(ModelObj* file, Graphics*& gfx, Camera*& cam, Mouse* mouse, Keyboard* keyboard, Hud* HUD, vec3 pos = vec3(0, 0, 0), vec3 rot = vec3(0, 0, 0), vec3 scale = vec3(1, 1, 1));
+	Player(ModelObj* file, Graphics*& gfx, Camera*& cam, Mouse* mouse, Keyboard * keyboard, Hud* HUD, vec3 pos = vec3(0, 0, 0), vec3 rot = vec3(0, 0, 0), vec3 scale = vec3(1, 1, 1));
 	virtual ~Player();
 	void update(float dt) override;
 	void handleEvents(float dt);
